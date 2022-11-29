f57b1036611b19be8741757580dc3817e8c43737
Tauhid <m.tah69@gmail.com>
2022-11-29 18:59:11 +0600

    video 11 done

--- djangoOne/accounts/urls.py
+++ djangoOne/accounts/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('products/', views.products, name='products'),
     path('customer/<str:pk>/', views.customer, name='customer'),
+    path('create-order/', views.createOrder, name='createOrder'),
+    path('update-order/<str:pk>/', views.updateOrder, name='updateOrder'),
+    path('delete-order/<str:pk>/', views.deleteOrder, name='deleteOrder'),
+    
 ]
